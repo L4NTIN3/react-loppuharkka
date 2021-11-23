5ad12e131a5b4130a46e71bc0163f4f3e3f19a80
Tommi Virtanen <tommiv01@outlook.com>
2021-11-24 01:26:09 +0200

    h

--- lopputekele/src/Valikko.js
+++ lopputekele/src/Valikko.js
@@ -6,7 +6,7 @@ const Menu = () => {
             <button>Fetch 1 quote</button>
             <button>Delete 1 quote</button>
             <input></input>
-            <p>näin monta näytetään </p>
+            <p>näin monta  </p>
             <p> näin monta on tallennettu </p>
         </div>
     )
