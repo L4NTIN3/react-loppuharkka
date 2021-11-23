9d4135e5aa9090e2eb9947effc976e7d30cc5085
Tommi Virtanen <tommiv01@outlook.com>
2021-11-24 01:22:45 +0200

    Jos nyt sitte ei pelleiltäs

---
+++ lopputekele/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from './Valikko';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      
+      <Menu />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ lopputekele/src/Valikko.js
@@ -0,0 +1,15 @@
+const Menu = () => {
+    return(
+        <div>
+            <h1>Kanye Oneliners</h1>
+
+            <button>Fetch 1 quote</button>
+            <button>Delete 1 quote</button>
+            <input></input>
+            <p>näin monta näytetään </p>
+            <p> näin monta on tallennettu </p>
+        </div>
+    )
+}
+
+export default Menu;
