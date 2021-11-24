ff1c680d69f53d427c75e661ac9275de995be98e
Tommi Virtanen <tommiv01@outlook.com>
2021-11-24 21:18:41 +0200

    aloitettu

--- lopputekele/src/Valikko.js
+++ lopputekele/src/Valikko.js
@@ -1,14 +1,34 @@
 const Menu = () => {
     return(
-        <div>
+
+        <div className = "w-full">
+
             <h1 className="font-bold">Kanye Oneliners</h1>
 
-            <button>Fetch 1 quote</button>
-            <button>Delete 1 quote</button>
-            <input></input>
-            <p>näin monta  </p>
-            <p> näin monta on tallennettu </p>
+            <div className="h-48 flex flex-wrap content-start bg-gray-500">
+                
+                <div className=" h-30 flex flex-col flex-wrap w-1/4 m-5">
+                    <button className="btn btn-green">Fetch 1 quote</button>
+                    <button className="btn btn-green">Delete 1 quote</button>
+
+                </div>
+                <div className=" h-10 flex flex-wrap w-1/4 m-5">
+                    <input></input>
+                </div>
+
+
+                <div className="flex flex-col flex-wrap w-1/4 m-5">
+                    <p>Fetch 1 quote</p>
+                    <p>Delete 1 quote</p>
+
+                </div>
+
+                
+            </div>
+
         </div>
+
+        
     )
 }
 
